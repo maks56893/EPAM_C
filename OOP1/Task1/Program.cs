54065736a3fd90fba2be70129910ec5605f07f10
Maxim <dget2013@gmail.com>
2020-11-22 22:30:46 +0300

    OOP1

---
+++ OOP1/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Round round = new Round(5);
+            round.GetInfo();
+
+            rectangle rct = new rectangle(5, 10);
+            rct.GetInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ OOP1/Task1/rectangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class rectangle : Figure
+    {
+        private float a, b;
+        public float A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    a = value;
+                }
+                else
+                {
+                    Console.WriteLine("Длина стороны не может быть отрицательной");
+                }
+            }
+        }
+        public float B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    b = value;
+                }
+                else
+                {
+                    Console.WriteLine("Длина стороны не может быть отрицательной");
+                }
+            }
+        }
+        public override double Square()
+        {
+            double S = a * b;
+            return S;
+        }
+
+        public override double Perimeter()
+        {
+            double P = 2 * (a + b);
+            return P;
+        }
+
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Сторона А = {a},сторона В = {b}, Площадь = {Square()}, длина окружности = {Perimeter()}");
+        }
+
+        public rectangle(float A, float B)
+        {
+            this.A = A;
+            this.B = B;
+        }
+
+    }
+}
