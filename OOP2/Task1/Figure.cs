83fa258f84b894c719b4d5ced2567785ca60f6a5
Maxim <dget2013@gmail.com>
2020-12-01 21:47:26 +0300

    OOP2

---
+++ OOP2/Task1/Figure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public abstract class Figure
+    {
+       abstract public double Square();
+       abstract public double Perimeter();
+    }
+}
