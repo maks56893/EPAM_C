c9a6dec92cb1f89a243263c70d3201bffce90d67
Maxim <dget2013@gmail.com>
2020-12-08 00:40:36 +0300

    Collections

---
+++ Collections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phones p1 = new Phones(1, "Apple", "USA");
+            Phones p2 = new Phones(2, "Samsug", "Korea");
+            Phones p3 = new Phones(3, "Sony", "Japan");
+            Phones p4 = new Phones(4, "Xiaomi", "China");
+            Phones p5 = new Phones(5, "Motorola", "USA");
+            Phones p6 = new Phones(6, "Google", "USA");
+
+            List<Phones> temp = new List<Phones>() { p1, p2, p3, p4, p5, p6 };
+            List<Phones> temp2 = temp.Where(x => x.counry == "USA").ToList();
+
+            try
+            {
+                Console.WriteLine("Введите целое число");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("------------------");
+                Console.WriteLine(x);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Ошибка. Введены данные не целочисленного типа");
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- Collections/Properties/AssemblyInfo.cs
+++ Collections/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("Task1")]
+[assembly: AssemblyTitle("Collections")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1")]
+[assembly: AssemblyProduct("Collections")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("e5222991-37b3-49b6-bbbf-b4038208510b")]
+[assembly: Guid("d14068c6-c569-44dd-af64-662659894021")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
